fcf657be48cbfc79f46e18d4f9ae0c1d830f0312
Q小白 <zhengyc9090@163.com>
2018-06-14 14:32:01 +0800

    初始化

--- src/emqttd_plugin_kafka_bridge.erl
+++ src/emqttd_plugin_kafka_bridge.erl
@@ -35,87 +35,83 @@ load(Env) ->
     ekaf_init([Env]),
     emqttd:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]),
     emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
-    emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/4, [Env]),
-    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4, [Env]),
-    emqttd:hook('session.created', fun ?MODULE:on_session_created/3, [Env]),
-    emqttd:hook('session.subscribed', fun ?MODULE:on_session_subscribed/4, [Env]),
-    emqttd:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4, [Env]),
-    emqttd:hook('session.terminated', fun ?MODULE:on_session_terminated/4, [Env]),
+    emqttd:hook('client.subscribe', fun ?MODULE:on_client_subscribe/3, [Env]),
+    emqttd:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/3, [Env]),
+    emqttd:hook('session.created', fun ?MODULE:on_session_created/2, [Env]),
+    emqttd:hook('session.subscribed', fun ?MODULE:on_session_subscribed/3, [Env]),
+    emqttd:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/3, [Env]),
+    emqttd:hook('session.terminated', fun ?MODULE:on_session_terminated/3, [Env]),
     emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
-    emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/4, [Env]),
-    emqttd:hook('message.acked', fun ?MODULE:on_message_acked/4, [Env]).
+    emqttd:hook('message.delivered', fun ?MODULE:on_message_delivered/3, [Env]),
+    emqttd:hook('message.acked', fun ?MODULE:on_message_acked/3, [Env]).
 
 on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) ->
     io:format("client ~s connected, connack: ~w~n", [ClientId, ConnAck]),
-    io:format("**************************************1: ~w~n", [ClientId]),
+
+    ekaf_send(<<"connected">>, ClientId, {}, _Env),
+
     {ok, Client}.
 
 on_client_disconnected(Reason, _Client = #mqtt_client{client_id = ClientId}, _Env) ->
     io:format("client ~s disconnected, reason: ~w~n", [ClientId, Reason]),
-    io:format("**************************************2: ~w~n", [ClientId]),
+    ekaf_send(<<"connected">>, ClientId, {}, _Env),
     ok.
 
-on_client_subscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("client(~s/~s) will subscribe: ~p~n", [Username, ClientId, TopicTable]),
-    io:format("**************************************3: ~w~n", [ClientId]),
+on_client_subscribe(ClientId, TopicTable, _Env) ->
+    io:format("client(~s/~s) will subscribe: ~p~n", [ClientId, TopicTable]),
     {ok, TopicTable}.
 
-on_client_unsubscribe(ClientId, Username, TopicTable, _Env) ->
-    io:format("client(~s/~s) unsubscribe ~p~n", [ClientId, Username, TopicTable]),
-    io:format("**************************************4: ~w~n", [ClientId]),
-    {ok, TopicTable}.
+on_client_unsubscribe(ClientId, Topics, _Env) ->
+    io:format("client(~s/~s) unsubscribe ~p~n", [ClientId, Topics]),
+    {ok, Topics}.
 
-on_session_created(ClientId, Username, _Env) ->
-    io:format("**************************************5: ~w~n", [ClientId]),
-    io:format("session(~s/~s) created.", [ClientId, Username]).
+on_session_created(ClientId, _Env) ->
+    io:format("session(~s/~s) created.", [ClientId]).
 
-on_session_subscribed(ClientId, Username, {Topic, Opts}, _Env) ->
-    io:format("**************************************6: ~w~n", [ClientId]),
-    io:format("session(~s/~s) subscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
+on_session_subscribed(ClientId, {Topic, Opts}, _Env) ->
+    io:format("session(~s/~s) subscribed: ~p~n", [ClientId, {Topic, Opts}]),
+    ekaf_send(<<"subscribed">>, ClientId, {Topic, Opts}, _Env),
     {ok, {Topic, Opts}}.
 
-on_session_unsubscribed(ClientId, Username, {Topic, Opts}, _Env) ->
-    io:format("**************************************7: ~w~n", [ClientId]),
-    io:format("session(~s/~s) unsubscribed: ~p~n", [Username, ClientId, {Topic, Opts}]),
+on_session_unsubscribed(ClientId, {Topic, Opts}, _Env) ->
+    io:format("session(~s/~s) unsubscribed: ~p~n", [ClientId, {Topic, Opts}]),
+    ekaf_send(<<"unsubscribed">>, ClientId, {Topic, Opts}, _Env),
     ok.
 
-on_session_terminated(ClientId, Username, Reason, _Env) ->
-    io:format("**************************************8: ~w~n", [ClientId]),
-    io:format("session(~s/~s) terminated: ~p.", [ClientId, Username, Reason]).
+on_session_terminated(ClientId, Reason, _Env) ->
+    io:format("session(~s/~s) terminated: ~p.", [ClientId, Reason]),
+    stop.
 
 %% transform message and return
 on_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _Env) ->
-    io:format("**************************************9: ~w~n", [emqttd_message:format(Message)]),
     {ok, Message};
 
 on_message_publish(Message, _Env) ->
-    io:format("**************************************10: ~w~n", [emqttd_message:format(Message)]),
     io:format("publish ~s~n", [emqttd_message:format(Message)]),
+    ekaf_send(<<"public">>, {}, Message, _Env),
     {ok, Message}.
 
-on_message_delivered(ClientId, Username, Message, _Env) ->
-    io:format("**************************************11: ~w~n", [ClientId]),
-    io:format("delivered to client(~s/~s): ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
+on_message_delivered(ClientId, Message, _Env) ->
+    io:format("delivered to client(~s/~s): ~s~n", [ClientId, emqttd_message:format(Message)]),
     {ok, Message}.
 
-on_message_acked(ClientId, Username, Message, _Env) ->
-    io:format("**************************************12: ~w~n", [ClientId]),
-    io:format("client(~s/~s) acked: ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
+on_message_acked(ClientId, Message, _Env) ->
+    io:format("client(~s/~s) acked: ~s~n", [ClientId, emqttd_message:format(Message)]),
     {ok, Message}.
 
 %% Called when the plugin application stop
 unload() ->
     emqttd:unhook('client.connected', fun ?MODULE:on_client_connected/3),
     emqttd:unhook('client.disconnected', fun ?MODULE:on_client_disconnected/3),
-    emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/4),
-    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/4),
-    emqttd:unhook('session.created', fun ?MODULE:on_session_created/3),
-    emqttd:unhook('session.subscribed', fun ?MODULE:on_session_subscribed/4),
-    emqttd:unhook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4),
-    emqttd:unhook('session.terminated', fun ?MODULE:on_session_terminated/4),
+    emqttd:unhook('client.subscribe', fun ?MODULE:on_client_subscribe/3),
+    emqttd:unhook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/3),
+    emqttd:unhook('session.created', fun ?MODULE:on_session_created/2),
+    emqttd:unhook('session.subscribed', fun ?MODULE:on_session_subscribed/3),
+    emqttd:unhook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/3),
+    emqttd:unhook('session.terminated', fun ?MODULE:on_session_terminated/3),
     emqttd:unhook('message.publish', fun ?MODULE:on_message_publish/2),
-    emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/4),
-    emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/4).
+    emqttd:unhook('message.delivered', fun ?MODULE:on_message_delivered/3),
+    emqttd:unhook('message.acked', fun ?MODULE:on_message_acked/3).
 
 %% ==================== ekaf_init STA.===============================%%
 ekaf_init(_Env) ->
@@ -137,3 +133,95 @@ ekaf_init(_Env) ->
 
     io:format("Init ekaf with ~p~n", [BootstrapBroker]).
 %% ==================== ekaf_init END.===============================%%
+
+
+%% ==================== ekaf_send STA.===============================%%
+ekaf_send(Type, ClientId, {}, _Env) ->
+    Json = mochijson2:encode([
+        {type, Type},
+        {client_id, ClientId},
+        {message, {}},
+        {cluster_node, node()},
+        {ts, emqttd_time:now_ms()}
+    ]),
+    ekaf_send_sync(Json);
+ekaf_send(Type, ClientId, {Reason}, _Env) ->
+    Json = mochijson2:encode([
+        {type, Type},
+        {client_id, ClientId},
+        {cluster_node, node()},
+        {message, Reason},
+        {ts, emqttd_time:now_ms()}
+    ]),
+    ekaf_send_sync(Json);
+ekaf_send(Type, ClientId, {Topic, Opts}, _Env) ->
+    Json = mochijson2:encode([
+        {type, Type},
+        {client_id, ClientId},
+        {cluster_node, node()},
+        {message, [
+            {topic, Topic},
+            {opts, Opts}
+        ]},
+        {ts, emqttd_time:now_ms()}
+    ]),
+    ekaf_send_sync(Json);
+ekaf_send(Type, _, Message, _Env) ->
+    Id = Message#mqtt_message.id,
+    From = Message#mqtt_message.from, %需要登录和不需要登录这里的返回值是不一样的
+    Topic = Message#mqtt_message.topic,
+    Payload = Message#mqtt_message.payload,
+    Qos = Message#mqtt_message.qos,
+    Dup = Message#mqtt_message.dup,
+    Retain = Message#mqtt_message.retain,
+    Timestamp = Message#mqtt_message.timestamp,
+
+    ClientId = c(From),
+    Username = u(From),
+
+    Json = mochijson2:encode([
+        {type, Type},
+        {client_id, ClientId},
+        {message, [
+            {username, Username},
+            {topic, Topic},
+            {payload, Payload},
+            {qos, i(Qos)},
+            {dup, i(Dup)},
+            {retain, i(Retain)}
+        ]},
+        {cluster_node, node()},
+        {ts, emqttd_time:now_ms()}
+    ]),
+    ekaf_send_sync(Json).
+
+ekaf_send_async(Msg) ->
+    Topic = ekaf_get_topic(),
+    ekaf_send_async(Topic, Msg).
+ekaf_send_async(Topic, Msg) ->
+    ekaf:produce_async_batched(list_to_binary(Topic), list_to_binary(Msg)).
+ekaf_send_sync(Msg) ->
+    Topic = ekaf_get_topic(),
+    ekaf_send_sync(Topic, Msg).
+ekaf_send_sync(Topic, Msg) ->
+    ekaf:produce_sync_batched(list_to_binary(Topic), list_to_binary(Msg)).
+
+i(true) -> 1;
+i(false) -> 0;
+i(I) when is_integer(I) -> I.
+c({ClientId, Username}) -> ClientId;
+c(From) -> From.
+u({ClientId, Username}) -> Username;
+u(From) -> From.
+%% ==================== ekaf_send END.===============================%%
+
+%% ==================== ekaf_set_topic STA.===============================%%
+ekaf_set_topic(Topic) ->
+    application:set_env(ekaf, ekaf_bootstrap_topics, list_to_binary(Topic)),
+    ok.
+ekaf_get_topic() ->
+    Env = application:get_env(emqttd_plugin_kafka_bridge, kafka),
+    {ok, Kafka} = Env,
+    Topic = proplists:get_value(topic, Kafka),
+    Topic.
+%% ==================== ekaf_set_topic END.===============================%%
